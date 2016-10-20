a77fe95fa208edcaf961ee9de776e804c9535116
Zhe <zhe@Zhes-MacBook-Pro.local>
2016-10-19 23:36:32 -0700

    Add vuex

--- src/components/ElementTable.vue
+++ src/components/ElementTable.vue
@@ -1,28 +1,23 @@
 <template>
   <el-table
-    :data="tableData3"
-    selection-mode="multiple"
-    style="width: 100%"
-    @selectionchange="handleMultipleSelectionChange">
+    :data="tableData"
+    border
+    style="width: 100%">
     <el-table-column
-      type="selection"
-      width="50">
-    </el-table-column>
-    <el-table-column
-      inline-template
+      property="date"
       label="日期"
-      width="120">
-      <div>{{ row.date }}</div>
+      sortable
+      width="180">
     </el-table-column>
     <el-table-column
       property="name"
       label="姓名"
-      width="120">
+      width="180">
     </el-table-column>
     <el-table-column
       property="address"
       label="地址"
-      show-tooltip-when-overflow>
+      :formatter="formatter">
     </el-table-column>
   </el-table>
 </template>
@@ -31,42 +26,28 @@
   export default {
     data() {
       return {
-        tableData3: [{
-          date: '2016-05-03',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-        }, {
+        tableData: [{
           date: '2016-05-02',
           name: '王小虎',
           address: '上海市普陀区金沙江路 1518 弄'
         }, {
           date: '2016-05-04',
           name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
+          address: '上海市普陀区金沙江路 1517 弄'
         }, {
           date: '2016-05-01',
           name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
+          address: '上海市普陀区金沙江路 1519 弄'
         }, {
-          date: '2016-05-08',
+          date: '2016-05-03',
           name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-        }, {
-          date: '2016-05-06',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-        }, {
-          date: '2016-05-07',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-        }],
-        multipleSelection: []
+          address: '上海市普陀区金沙江路 1516 弄'
+        }]
       }
     },
-
     methods: {
-      handleSelectionChange (val) {
-        this.multipleSelection = val
+      formatter(row, column) {
+        return row.address;
       }
     }
   }
--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import App from './App'
 import Element from 'element-ui'
+import Vuex from 'vuex'
 Vue.use(Element)
-
+Vue.use(Vuex)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
